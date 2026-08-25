72176dc8c06c76f8610bbaf8b4d91a69f2ccd71c
Anshu Tiwari <anshutiwari4141@gmail.com>
2026-08-26 01:18:44 +0530

    BankingDB.sql First file uploaded

---
+++ Banking_DB.sql
@@ -0,0 +1,49 @@
+create database BankingDB;
+use BankingDB;
+CREATE TABLE Customers
+(
+    CustomerID INT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15)
+);
+
+describe Customers;
+
+CREATE TABLE Accounts (
+    AccountID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2)
+);
+CREATE TABLE Transactions (
+    TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+);
+
+CREATE TABLE Branches (
+    BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+
+CREATE TABLE AccountBranches ( 
+		AssignmentDate DATE
+);
+CREATE TABLE Loans (
+    LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE
+);
+
+Describe Accounts;
+Describe Transactions;
+describe Branches;
+describe AccountBranches;
+describe Loans;
+
